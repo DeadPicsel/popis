e8cd2b1a32a33dedfc1c17300d239b561d01f981
DeadPicsel <dannyboi1903@gmail.com>
2022-06-19 17:01:28 +0100

    MKI
    Fist version of script

---
+++ script.sh
@@ -0,0 +1,7 @@
+# Dock Modifications
+
+## Transparency Mod
+###### Set Transparency Mode to Fixed
+
+###### Set Opacity to 80%
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.2
